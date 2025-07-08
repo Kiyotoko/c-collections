e60cc605211b689190d45dac5468e7b1ac0ecf07
Kiyotoko <karl.zschiebsch@gmail.com>
2025-07-08 13:30:01 +0200

    test: update assertations api

--- src/assertations.c
+++ src/assertations.c
@@ -1,26 +1,66 @@
 #include "assertations.h"
 
+#define CSI "\e["
+#define ANSI_RESET CSI "0m"
+#define ANSI_BOLD CSI "1m"
+#define ANSI_RED CSI "91m"
+
+void fail_with(const char* str) {
+    fprintf(stderr, "%s%s%s%s\n", ANSI_RED, ANSI_BOLD, str, ANSI_RESET);
+    fail;
+}
+
+void assert_equalsi_with(int a, int b, const char* str) {
+    if (a != b) {
+        fail_with(str);
+    }
+}
+void assert_equalsl_with(long a, long b, const char* str) {
+    if (a != b) {
+        fail_with(str);
+    }
+}
+void assert_equalsf_with(float a, float b, const char* str) {
+    if (a != b) {
+        fail_with(str);
+    }
+}
+void assert_equalsb_with(bool a, bool b, const char* str) {
+    if (a != b) {
+        fail_with(str);
+    }
+}
+void assert_equalsp_with(void* a, void* b, const char* str) {
+    if (a != b) {
+        fail_with(str);
+    }
+}
+
 void assert_equalsi(int a, int b) {
     if (a != b) {
-        fprintf(stderr, "Expected %i, got %i\n", a, b);
+        fprintf(stderr, "%s%sExpected %i, got %i%s\n",
+            ANSI_RED, ANSI_BOLD, a, b, ANSI_RESET);
         fail;
     }
 }
 void assert_equalsl(long a, long b) {
     if (a != b) {
-        fprintf(stderr, "Expected %i, got %i\n", a, b);
+        fprintf(stderr, "%s%sExpected %li, got %li%s\n",
+            ANSI_RED, ANSI_BOLD, a, b, ANSI_RESET);
         fail;
     }
 }
 void assert_equalsf(float a, float b) {
     if (a != b) {
-        fprintf(stderr, "Expected %f, got %f\n", a, b);
+        fprintf(stderr, "%s%sExpected %f, got %f%s\n",
+            ANSI_RED, ANSI_BOLD, a, b, ANSI_RESET);
         fail;
     }
 }
 void assert_equalsb(bool a, bool b) {
     if (a != b) {
-        fprintf(stderr, "Expected %i, got %i\n", a, b);
+        fprintf(stderr, "%s%sExpected %s, got %s%s\n",
+            ANSI_RED, ANSI_BOLD, a ? "true" : "false", b ? "true" : "false", ANSI_RESET);
         fail;
     }
 }
@@ -28,5 +68,27 @@ void assert_equalsp(void* a, void* b) {
     if (a != b) {
         fprintf(stderr, "Expected %p, got %p\n", a, b);
         fail;
+    }
+}
+
+void assert_true_with(bool v, const char* str) {
+    if (!v) {
+        fail_with(str);
+    }
+}
+void assert_false_with(bool v, const char* str) {
+    if (!v) {
+        fail_with(str);
+    }
+}
+
+void assert_true(bool v) {
+    if (!v) {
+        fail_with("Expected true, got false");
+    }
+}
+void assert_false(bool v) {
+    if (v) {
+        fail_with("Expected false, got true");
     }
 }
--- src/assertations.h
+++ src/assertations.h
@@ -7,6 +7,22 @@
 
 #define fail exit(1)
 
+void fail_with(const char* str);
+
+void assert_equalsi_with(int a, int b, const char* str);
+void assert_equalsl_with(long a, long b, const char* str);
+void assert_equalsf_with(float a, float b, const char* str);
+void assert_equalsb_with(bool a, bool b, const char* str);
+void assert_equalsp_with(void* a, void* b, const char* str);
+
+#define assert_equals_with(X, Y, Z) _Generic((X), \
+    int: assert_equalsi_with, \
+    long: assert_equalsl_with, \
+    float: assert_equalsf_with, \
+    bool: assert_equalsb_with, \
+    default: assert_equalsp_with \
+)(X, Y, Z)
+
 void assert_equalsi(int a, int b);
 void assert_equalsl(long a, long b);
 void assert_equalsf(float a, float b);
@@ -21,5 +37,10 @@ void assert_equalsp(void* a, void* b);
     default: assert_equalsp \
 )(X, Y)
 
+void assert_true_with(bool v, const char* str);
+void assert_false_with(bool v, const char* str);
+
+void assert_true(bool v);
+void assert_false(bool v);
 
 #endif
